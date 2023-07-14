376bdfd0ecb4bf7138b949643e610963d6e44ce4
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-15 01:51:36 +0600

    feat & fix: navbar added & bug fix

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 export default function Layout() {
   return (
     <div className="h-screen flex flex-col justify-between">
+      <Navbar />
       <div className="mb-auto">
         <Outlet />
       </div>
