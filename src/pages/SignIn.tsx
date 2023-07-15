d4be0c7268b4c3de1aa046ccb3877f22216c1580
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-15 22:32:31 +0600

    feat: sign in and sign up page done

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -12,22 +12,6 @@ const SignIn = () => {
             <div className="p-2 w-full">
               <div className="relative">
                 <label
-                  htmlFor="name"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-            </div>
-            <div className="p-2 w-full">
-              <div className="relative">
-                <label
                   htmlFor="email"
                   className="leading-7 text-sm text-gray-600"
                 >
@@ -37,6 +21,22 @@ const SignIn = () => {
                   type="email"
                   id="email"
                   name="email"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+            <div className="p-2 w-full">
+              <div className="relative">
+                <label
+                  htmlFor="password"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
                   className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </div>
