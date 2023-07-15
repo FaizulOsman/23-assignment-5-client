6a6ebb9fd0098c3306a104104082f52487e661f1
FaizulOsman <faizulosmanrabby@gmail.com>
2023-07-16 01:22:38 +0600

    feat: data load dynamically in all books page

--- src/redux/features/books/bookApi.ts
+++ src/redux/features/books/bookApi.ts
@@ -1,15 +1,4 @@
 import { api } from "../../api/apiSlice";
-console.log(api);
-
-// const bookApi = api.injectEndpoints({
-//   endpoints: (builder) => ({
-//     getAllBook: builder.query<IResponse, undefined>({
-//       query: () => ({ url: "/book" }),
-//     }),
-//   }),
-// });
-
-// export const { useGetAllBookQuery } = bookApi;
 
 const bookApi = api.injectEndpoints({
   endpoints: (builder) => ({
